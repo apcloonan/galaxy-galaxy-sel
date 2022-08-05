e1b8f2d59b4e4712e14183aec51f1b2fd129309e
aidan <apcloonan@gmail.com>
2022-08-05 17:34:46 -0500

    my utilities

---
+++ aidan_utils.py
@@ -0,0 +1,55 @@
+### Aidan's Utilities
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import astropy.io.fits as fits
+from astropy.table import Table                    # Table data structure
+
+# the following commands make plots look better
+def plot_prettier(dpi=200, fontsize=10): 
+    plt.rcParams['figure.dpi']= dpi
+    plt.rc("savefig", dpi=dpi)
+    plt.rc('font', size=fontsize)
+    plt.rc('xtick', direction='in')
+    plt.rc('ytick', direction='in')
+    plt.rc('xtick.major', pad=5) 
+    plt.rc('xtick.minor', pad=5)
+    plt.rc('ytick.major', pad=5) 
+    plt.rc('ytick.minor', pad=5)
+    plt.rc('lines', dotted_pattern = [2., 2.])
+    # if you don't have LaTeX installed on your laptop and this statement 
+    # generates error, comment it out
+    plt.rc('text', usetex=True)
+    
+def read_fits_table(filename):
+    '''
+    reads and returns data in a table from a FITS file
+    '''
+    hdu = fits.open(filename)
+    data = Table(hdu[1].data)   # second index of the hdu corresponds to the data in my astropy table files
+    hdu.close()
+    
+    return data
+
+def read_fits_image(filename):
+    '''
+    reads and returns an image from a FITS file
+    '''
+    hdu = fits.open(filename)
+    data = hdu[0].data
+    header = hdu[0].header 
+    hdu.close()
+    
+    return data
+
+def get_fits_files(path):
+    '''
+    given a directory as an input, this returns the path + filename of all FITS files in that
+    directory
+    '''
+    files = np.array(os.listdir(path))
+    finds = [i for i, f in enumerate(files) if '.fits' in f]
+    files = files[finds]
+    return np.core.defchararray.add(np.full(len(files), path), files)
+    
